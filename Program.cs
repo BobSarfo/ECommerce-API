301e62648649ad7afb29d654f5d3d30794d5a1bc
BobSarfo <robert@phlo.io>
2023-12-21 14:43:05 +0000

    add global exception handling

--- Program.cs
+++ Program.cs
@@ -117,6 +117,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseMiddleware<ExceptionHandler>();
+
 app.UseCors();
 
 app.UseAuthentication();
---
+++ Services/ExceptionHandler.cs
@@ -0,0 +1,51 @@
+﻿using HubtelCommerce.Dtos;
+using System.Net;
+using System.Text.Json;
+
+namespace HubtelCommerce.Services
+{
+    public class ExceptionHandler
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger _logger;
+        private readonly IHostEnvironment _env;
+
+        public ExceptionHandler(RequestDelegate next, ILogger<ExceptionHandler> logger, IHostEnvironment env)
+        {
+            _next = next;
+            _logger = logger;
+            _env = env;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, ex.Message);
+                context.Response.ContentType = "application/json";
+                context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                var development = new ApiException(context.Response.StatusCode, ex.Message, ex.StackTrace?.ToString());
+                if (ex.InnerException != null)
+                {
+                    development.Details = ex.InnerException.StackTrace;
+                    development.Message = ex.InnerException.Message;
+                }
+                var production = new ApiException(context.Response.StatusCode, "Internal Server Error, An unexpected error occurred.Please try again later");
+                var response = _env.IsDevelopment() ? development : production;
+
+                if (_env.IsProduction()) 
+                {
+                    _logger.LogError(response.Message +" : "+response.Details);
+                }
+                var option = new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase };
+
+                var json = JsonSerializer.Serialize(response, option);
+                await context.Response.WriteAsync(json);
+            }
+        }
+    }
+}
